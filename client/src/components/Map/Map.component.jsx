fc2d2dc3c229f94417a0a3aa8707673b188104cc
z3by <zoaby.am@gmail.com>
2018-09-03 11:27:01 +0300

    ADD basic components, basic routing

---
+++ client/src/components/Map/Map.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Map.css';
+
+export default class Map extends Component {
+    render() {
+        return (
+            <div>
+                Map
+            </div>
+        )
+    }
+}
---
+++ client/src/components/Stories/Stories.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Stories.css';
+
+export default class Stories extends Component {
+    render() {
+        return (
+            <div>
+                stories
+            </div>
+        )
+    }
+}
