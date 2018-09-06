31c882ac4f1ece41753bdc00a34a6a409a6aaf65
z3by <zoaby.am@gmail.com>
2018-09-06 08:41:47 -0700

    UPDATE fix the merge issues

--- client/src/components/Router/Router.jsx
+++ client/src/components/Router/Router.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../../App.css";
 
 import About from "../About/About.component";
