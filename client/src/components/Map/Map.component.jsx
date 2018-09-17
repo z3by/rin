e759666333eeda90f566419a9f6fe0aa1a2258b1
z3by <zoaby.am@gmail.com>
2018-09-17 13:08:09 +0300

    FIX tooltip style

--- client/src/components/Map/Map.component.jsx
+++ client/src/components/Map/Map.component.jsx
@@ -1,31 +1,28 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 import "./Map.css";
-import * as options from './map-options';
-import * as projects from './projects-data.json';
+import * as options from "./map-options";
+import * as projects from "./projects-data.json";
 
-
-const Dot = (props) => {
+const Dot = props => {
   const colors = {
-    'housing': '#E83338',
-    'education': '#ff9068',
-    'agriculture': '#FFB75E',
-    'health': '#8DC26F',
-    'water': '#64b3f4',
-    'nutrition': '#6441A5',
-    'infancy': '#fc67fa',
-  }
+    housing: "#E83338",
+    education: "#ff9068",
+    agriculture: "#FFB75E",
+    health: "#8DC26F",
+    water: "#64b3f4",
+    nutrition: "#6441A5",
+    infancy: "#fc67fa"
+  };
   return (
     <div className="dot" style={{ background: colors[props.project.type] }}>
       <div class="tooltip">
-
-        <div class="tooltiptext spec">
-          <img src="https://source.unsplash.com/random" />
-          {props.project.title}
-        </div>
+        <div class="tooltiptext">{props.project.title}</div>
+        <img src="https://source.unsplash.com/random" />
+        <p className="tooltip-money">{props.project.intensity}$</p>
       </div>
     </div>
-  )
+  );
 };
 
 export default class Map extends Component {
@@ -35,12 +32,11 @@ export default class Map extends Component {
       lng: 35.99
     },
     zoom: 5,
-    currentType: 'agriculture'
+    currentType: "agriculture"
   };
 
   componentWillMount() {
     this.getUserLocation();
-
   }
 
   // get the user location;
@@ -56,33 +52,37 @@ export default class Map extends Component {
   };
 
   // mouse hover handler for the spectrum;
-  handleMouseHover = (e) => {
+  handleMouseHover = e => {
     const index = e.target.dataset.index;
     const types = [
-      'housing',
-      'education',
-      'agriculture',
-      'health',
-      'water',
-      'nutrition',
-      'infancy'
-    ]
+      "housing",
+      "education",
+      "agriculture",
+      "health",
+      "water",
+      "nutrition",
+      "infancy"
+    ];
 
     this.setState({
       currentType: types[index]
     });
-
-  }
+  };
 
   render() {
-
     const dots = projects.map((project, key) => {
       return (
-        this.state.currentType === project.type && <Dot lng={project.position.lng} lat={project.position.lat} key={key} project={project} key={key} />
+        this.state.currentType === project.type && (
+          <Dot
+            lng={project.position.lng}
+            lat={project.position.lat}
+            key={key}
+            project={project}
+            key={key}
+          />
+        )
       );
-    })
-
-
+    });
 
     return (
       <div
@@ -91,39 +91,65 @@ export default class Map extends Component {
       >
         <div className="filter-spectrum">
           <ul className="spectrum">
-            <li className="spectrum-item" data-index={0} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={0}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">housing projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={1} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={1}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
-                <div class="tooltiptext spec">
-                  education projects
-                </div>
+                <div class="tooltiptext spec">education projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={2} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={2}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">agriculture projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={3} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={3}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">health projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={4} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={4}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">water projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={5} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={5}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">nutrition projects</div>
               </div>
             </li>
-            <li className="spectrum-item" data-index={6} onMouseEnter={this.handleMouseHover}>
+            <li
+              className="spectrum-item"
+              data-index={6}
+              onMouseEnter={this.handleMouseHover}
+            >
               <div class="tooltip">
                 <div class="tooltiptext spec">infancy projects</div>
               </div>
@@ -138,14 +164,7 @@ export default class Map extends Component {
         >
           {[dots]}
         </GoogleMapReact>
-      </div >
+      </div>
     );
   }
 }
-
-
-
-
-
-
-
