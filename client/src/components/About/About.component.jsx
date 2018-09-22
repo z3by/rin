22f51d364eaf4301240c12ccb7e70515e7df6695
ShSukkar <sukkarsmiling89@hotmail.com>
2018-09-22 11:37:39 +0300

    UPDATE About component

--- client/src/components/About/About.component.jsx
+++ client/src/components/About/About.component.jsx
@@ -49,108 +49,96 @@ export default class About extends Component {
   render() {
     return (
       <div className="about fadeInFast">
-        <div className="pages">
-          <div className="page1">
-            <img src="/imgs/about1.jpg" className="about1-img" alt="" />
-            <div className="about1-text">
-              <h2>About</h2>
-              <br />
-              <p>
-                The RIN moves private capital from commitment to active
-                investment by sourcing, structuring, and supporting the
-                financing of projects and companies that benefit refugees and
-                host communities.
-              </p>
-            </div>
+        <img src="/imgs/about1.jpg" className="banner-img" />
+        <div className="container">
+
+          <div className="about-header">
+            <h1 className="heading-theme-1">About</h1>
+            <p className="p-theme-1">
+              The RIN moves private capital from commitment to active
+              investment by sourcing, structuring, and supporting the
+              financing of projects and companies that benefit refugees and
+              host communities.
+          </p>
+
           </div>
-          <div className="page2">
-            <div className="row">
-              <div className="column">
-                <div className="card">
-                  <img
-                    src="/imgs/John.jpg"
-                    alt="Jane"
-                    style={{ width: "100%" }}
-                  />
-                  <div className="container">
-                    <h2 className="team-member-name">JOHN W. KLUGE</h2>
-                    <p className="team-member-title">
-                      Founder + Managing Director
+
+          <img src="/imgs/about2.jpg" className="about1-img" />
+          <h2 className="heading-theme-2">The RIN strategy has three pillars:</h2>
+          <p className="p-theme-1">
+            <b>Research:</b> Create the first investor-centered knowledge hub targeting business opportunities that support refugee self-reliance <br />
+            <b>Facilitation:</b> Build a pipeline of bankable deals that will speed and scale private investment in communities of displaced people <br />
+            <b>Policy and Advocacy:</b> Articulate and bridge investor needs to funders, governments, and the development community to drive catalytic investments and policies <br />
+          </p>
+
+          <div className="team">
+
+            <h1 className="heading-theme-1">meet the team</h1>
+            <div className="team-members">
+              <div className="team-member">
+                <img
+                  src="/imgs/John.jpg"
+                  alt="Jane"
+                  style={{ width: "100%" }}
+                />
+                <h2 className="heading-theme-2">JOHN W. KLUGE</h2>
+                <h3 className="heading-theme-3">
+                  Founder + Managing Director
+                    </h3>
+                <p className="p-theme-1">
+                  John W. Kluge is a systems entrepreneur and committed
+                  social justice activist. He is the Co-Founder and Managing
+                  Partner of the Alight Fund, an investment and financing
+                  company for refugee and host country entrepreneurs.
+                  Previously, he co-founded Eirene, a multi-family-office
+                  impact investing fund, Toilet Hackers, a social enterprise
+                  dedicated to scaling access to dignified sanitation for
+                  the 2.5 billion people without a toilet
                     </p>
-                    <p className="team-member-data">
-                      John W. Kluge is a systems entrepreneur and committed
-                      social justice activist. He is the Co-Founder and Managing
-                      Partner of the Alight Fund, an investment and financing
-                      company for refugee and host country entrepreneurs.
-                      Previously, he co-founded Eirene, a multi-family-office
-                      impact investing fund, Toilet Hackers, a social enterprise
-                      dedicated to scaling access to dignified sanitation for
-                      the 2.5 billion people without a toilet
-                    </p>
-                  </div>
-                </div>
               </div>
 
-              <div className="column">
-                <div className="card">
-                  <img
-                    src="/imgs/Tim.jpeg"
-                    alt="Mike"
-                    style={{ width: "100%" }}
-                  />
-                  <div className="container">
-                    <h2 className="team-member-name">
-                      TIMOTHY W. DOCKING, Ph.D
+              <div className="team-member">
+
+                <img
+                  src="/imgs/Tim.jpeg"
+                  alt="Mike"
+                  style={{ width: "100%" }}
+                />
+                <h2 className="heading-theme-2">
+                  TIMOTHY W. DOCKING, Ph.D
                     </h2>
-                    <p className="team-member-title">Managing Director</p>
-                    <p className="team-member-data">
-                      Timothy W. Docking, Ph.D. is a Washington, D.C.-based
-                      business executive with twenty years of emerging market
-                      and public sector experience. As an intrapreneur, he
-                      worked at the intersection of business, technology and
-                      development and built a new $100M revenue stream at IBM;
-                      as a public sector executive and manager, he helped start
-                      up the MCC.
+                <h3 className="heading-theme-3">Managing Director</h3>
+                <p className="p-theme-1">
+                  Timothy W. Docking, Ph.D. is a Washington, D.C.-based
+                  business executive with twenty years of emerging market
+                  and public sector experience. As an intrapreneur, he
+                  worked at the intersection of business, technology and
+                  development and built a new $100M revenue stream at IBM;
+                  as a public sector executive and manager, he helped start
+                  up the MCC.
                     </p>
-                  </div>
-                </div>
               </div>
-              <div className="column">
-                <div className="card">
-                  <img
-                    src="/imgs/Andrew.jpg"
-                    alt="John"
-                    style={{ width: "100%" }}
-                  />
-                  <div className="container">
-                    <h2 className="team-member-name">ANDREW STERN</h2>
-                    <p className="team-member-title">Strategic Advisor</p>
-                    <p className="team-member-data">
-                      Andrew Stern is the Founder, President and Executive
-                      Director of the Global Development Incubator (GDI) and a
-                      member of the GDI U.S. and Hong Kong boards. He leads the
-                      team across focus areas and crafts unconventional
-                      perspectives to drive the global development sector
-                      forward. Stern has played many roles within GDI
-                      initiatives, including Interim CEO of Convergence, the
-                      global network for Blended Finance
+              <div className="team-member">
+                <img
+                  src="/imgs/Andrew.jpg"
+                  alt="John"
+                  style={{ width: "100%" }}
+                />
+                <h2 className="heading-theme-2">ANDREW STERN</h2>
+                <h3 className="heading-theme-3">Strategic Advisor</h3>
+                <p className="p-theme-1">
+                  Andrew Stern is the Founder, President and Executive
+                  Director of the Global Development Incubator (GDI) and a
+                  member of the GDI U.S. and Hong Kong boards. He leads the
+                  team across focus areas and crafts unconventional
+                  perspectives to drive the global development sector
+                  forward. Stern has played many roles within GDI
+                  initiatives, including Interim CEO of Convergence, the
+                  global network for Blended Finance
                     </p>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-          {/* <div className="page3">
-            <img src="/imgs/about2.jpg" className="about2-img" />
-            <div className="about2-text">
-              <h2>The RIN strategy has three pillars:</h2>
-              <p>
-                <b>Research:</b> Create the first investor-centered knowledge hub targeting business opportunities that support refugee self-reliance <br />
-                <b>Facilitation:</b> Build a pipeline of bankable deals that will speed and scale private investment in communities of displaced people <br />
-                <b>Policy and Advocacy:</b> Articulate and bridge investor needs to funders, governments, and the development community to drive catalytic investments and policies <br />
-              </p>
-            </div>
-          </div> */}
         </div>
       </div>
     );
