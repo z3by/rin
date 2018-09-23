d78e58d74218d55811a3d7afca3fe6c6d28bd1bb
ShSukkar <sukkarsmiling89@hotmail.com>
2018-09-23 15:37:50 +0300

    UPDATE Story and Stories components

--- client/src/components/Story/Story.component.jsx
+++ client/src/components/Story/Story.component.jsx
@@ -7,8 +7,7 @@ export default class Story extends Component {
       {
         id: 0,
         title: "A Reason to Smile",
-        text:
-          'Husband and wife team Thierry Kouembi and Marlyse Tchuigoua know what it\'s like to be newcomers to Canada. They arrived in Calgary in 2015 from Italy with big dreams to bring Milan-quality fashion design and manufacturing to Alberta. Tchuigoua spent seven years as a pattern designer with the world-famous brand Dolce & Gabbana, following stints with other big names in Italy over 12 years after the couple moved there from Cameroon. "We decided it would be nice to come here and have our own manufacturing business," said Kouembi, who has also spent his life around clothes and fashion, working in fabric manufacturing himself as a student and being the son of a seamstress. The couple started small, working out of a room in their Calgary home, taking on all kinds of alterations and custom sewing jobs before deciding to take their dream to the next level. They did so with help from organizations like the Francophone Economic Development Council for Alberta and Futurpreneur Canada, a non-profit that provides financing and mentoring for entrepreneurs.',
+        text: 'A Social enterprise Smarita and her friend Becky began one of Kolkata’s first social enterprises tackling sex trafficking by employing rescued victims of sex trafficking. Reflection was created to employ sex trafficking survivors to manufacture fashion accessories and Destiny Foundation was created to utilise the profits from Reflection for various non-profit projects focussed on empowering girls and women.',
         img: "/imgs/story.jpeg"
       },
       {
@@ -61,8 +60,13 @@ export default class Story extends Component {
             <h1>{this.state.stories[id].title}</h1>
           </div>
         </div>
-        <p className="story-text">{this.state.stories[id].text}</p>
-        <div className="extra-space" />
+        <div className="story-images">
+          <img src="/imgs/RIN1.png" className="story-1" /><br />
+          <img src="/imgs/RIN2.jpg" />
+        </div>
+        <p className="story-text">
+          A Social enterprise Smarita and her friend Becky began one of Kolkata’s first social enterprises tackling sex trafficking by employing rescued victims of sex trafficking. Reflection was created to employ sex trafficking survivors to manufacture fashion accessories and Destiny Foundation was created to utilise the profits from Reflection for various non-profit projects focussed on empowering girls and women.',
+        </p>
       </div>
     );
   }
