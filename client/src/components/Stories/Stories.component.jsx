538327433a678df692a311ee47367023449120e1
z3by <zoaby.am@gmail.com>
2018-10-15 10:34:01 +0300

    UPDATE remove stories

--- client/src/components/Stories/Stories.component.jsx
+++ client/src/components/Stories/Stories.component.jsx
@@ -1,96 +1,38 @@
 import React, { Component } from "react";
 import "./Stories.css";
-import ScrollMagic from "scrollmagic";
-import { Link } from "react-router-dom";
 import * as stories from "./stories-info";
 
 class Stories extends Component {
-  state = {
-    videoURL: "/videos/AmbientWebDemo_06.mp4"
-  };
-
   componentDidMount() {
     document.body.style.overflowY = "auto";
-    var controller = new ScrollMagic.Controller();
-
-    for (let i = 1; i < 16; i++) {
-      new ScrollMagic.Scene({
-        triggerElement: ".link" + i
-      })
-        .setClassToggle(".link" + i, "show" + i)
-        .addTo(controller);
-    }
   }
 
-  onClickStory = e => {
-    console.log(e.target);
-  };
+  // handle the down arrow btn
+  goDown = () => {};
 
-  goDown = () => {
-    document.querySelector(".link10").scrollIntoView({
-      behavior: "smooth"
-    });
-  };
+  // handle the down arrow btn
+  goTop = () => {};
 
-  componentWillUnmount() {
-    document.body.style.overflowY = "hidden";
-  }
   render() {
     return (
       <div className="stories fadeInFast">
         <div className="header">
           <img src="/imgs/success.jpg" className="header-img" alt="" />
           <div className="header-text">
-            <h1>Success Stories</h1>
-            <a onClick={this.goDown} className="down-arrow">
-              <img src="/imgs/arrow.png" className="down-arrow-img" alt="" />
-            </a>
+            <h1 className="color-1">Success Stories</h1>
+
+            <div className="go-down" onClick={this.goDown}>
+              <i className="fas fa-arrow-circle-down" />
+            </div>
           </div>
         </div>
 
-        <div className="up" />
-
-        <ul className="stories-items">
-          {stories.map((story, i) => (
-            <li className="story-item">
-              <Link to={"/stories/" + story.id} className={"links link" + ++i}>
-                <span className="story-title">{story.title}</span>
-              </Link>
-            </li>
-          ))}
-
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link10 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link11 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link12 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link13 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link14 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-          <li className="story-item">
-            <Link to={"/stories/" + stories[4].id} className="link15 links">
-              <span className="story-title">{stories[4].title}</span>
-            </Link>
-          </li>
-        </ul>
+        <div className="story-items">
+          <div className="story" />
+        </div>
+        <div className="back-to-top" onClick={this.goTop}>
+          <i className="fas fa-arrow-circle-up" />
+        </div>
       </div>
     );
   }
