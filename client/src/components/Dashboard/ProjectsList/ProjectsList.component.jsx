93ac731b1811a3acff55d49a30c79cd160027a36
z3by <zoaby.am@gmail.com>
2018-10-18 15:46:56 +0300

    FEAT create project list component

--- client/src/components/Dashboard/ProjectsList/ProjectsList.component.jsx
+++ client/src/components/Dashboard/ProjectsList/ProjectsList.component.jsx
@@ -5,13 +5,43 @@ export default class ProjectsList extends Component {
   render() {
     return (
       <div>
-        <ul>
-          <li>6</li>
-          <li>6</li>
-          <li>6</li>
-          <li>6</li>
-          <li>6</li>
-        </ul>
+        <table class="projects-list-table">
+          <thead>
+            <tr>
+              <th>
+                <h1>Project ID</h1>
+              </th>
+              <th>
+                <h1>Project Name</h1>
+              </th>
+              <th>
+                <h1>Organization Name</h1>
+              </th>
+              <th>
+                <h1>Options</h1>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Google</td>
+              <td>9518</td>
+              <td>6369</td>
+              <td className="project-options">
+                <a>
+                  <i className="far fa-eye" /> show
+                </a>
+                <a>
+                  <i className="fas fa-edit" />
+                  update
+                </a>
+                <a>
+                  <i className="fas fa-trash-alt" /> delete
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
