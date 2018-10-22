495b3f95ff5a82efc6eb7cec5659b011d65589b5
z3by <zoaby.am@gmail.com>
2018-10-22 10:38:02 +0300

    merge

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const bodyParser = require('body-parser');
-const logger = require("morgan");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
@@ -16,7 +15,6 @@ migrateDB();
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(
@@ -34,12 +32,12 @@ app.get("*", (req, res) => {
   res.redirect("/");
 });
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
