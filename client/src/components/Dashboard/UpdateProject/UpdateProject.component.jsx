2db7c7b8d247b0876caaecd3328036d82a4f68e2
ShSukkar <sukkarsmiling89@hotmail.com>
2018-10-25 15:24:11 +0300

    FEAT add update project

---
+++ client/src/components/Dashboard/UpdateProject/UpdateProject.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./UpdateProject.css";
+
+export default class UpdateProject extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Update Project</h1>
+            </div>
+        )
+    }
+}
