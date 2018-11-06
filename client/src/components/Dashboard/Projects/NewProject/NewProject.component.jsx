8ebd1d81c9c92023133c6e1b08d42fbbc05c1e04
z3by <zoaby.am@gmail.com>
2018-11-06 09:23:29 +0200

    FIX react warnings

--- client/src/components/Dashboard/Projects/NewProject/NewProject.component.jsx
+++ client/src/components/Dashboard/Projects/NewProject/NewProject.component.jsx
@@ -190,7 +190,7 @@ export default class NewProject extends Component {
           <img
             className="admin-img-update"
             src={this.state.img_url}
-            alt="Project Image"
+            alt="Project uploaded"
           />
           <input
             type="file"
--- client/src/components/Members/Members.component.jsx
+++ client/src/components/Members/Members.component.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import "./Members.css";
 
 export default class Members extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.checkAuth();
   }
