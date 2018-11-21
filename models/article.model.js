826b7521c7fd187f80879667810034e481499cdf
z3by <zoaby.am@gmail.com>
2018-11-21 10:40:44 +0200

    article model

---
+++ models/article.model.js
@@ -0,0 +1,9 @@
+module.exports = `
+  CREATE TABLE IF NOT EXISTS articles (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    subtitle VARCHAR(3000) NOT NULL,
+    text VARCHAR(3000),
+    imgs JSON,
+    PRIMARY KEY (id),
+  )`;
