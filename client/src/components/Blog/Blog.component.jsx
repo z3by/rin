8e1ef99e45dd595af6b91709fb8feea557d60b44
z3by <zoaby.am@gmail.com>
2018-11-21 10:38:44 +0200

    add blog header

--- client/src/components/Blog/Blog.component.jsx
+++ client/src/components/Blog/Blog.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./Blog.css";
+import IconButton from "@material-ui/core/IconButton";
 
 export default class Blog extends Component {
   constructor() {
@@ -9,6 +10,18 @@ export default class Blog extends Component {
     };
   }
 
+  scrollToTop = () => {
+    document.querySelector(".library").scrollIntoView({
+      behavior: "smooth"
+    });
+  };
+
+  goDown = () => {
+    document.querySelector(".container").scrollIntoView({
+      behavior: "smooth"
+    });
+  };
+
   render() {
     return (
       <div
@@ -17,6 +30,18 @@ export default class Blog extends Component {
           overflowY: "scroll"
         }}
       >
+        <header>
+          <div className="banner-full">
+            <h1>blog</h1>
+            <div className="line" />
+            <h3>read the RIN latest articles</h3>
+            <div className="go-down" onClick={this.goDown}>
+              <IconButton>
+                <i className="fas fa-arrow-down color-2" />
+              </IconButton>
+            </div>
+          </div>
+        </header>
         <div className=" container ">
           <h1>hello from blog</h1>
         </div>
--- client/src/components/Stories/MoreStories/StoriesFilter/StoriesFilter.component.jsx
+++ client/src/components/Stories/MoreStories/StoriesFilter/StoriesFilter.component.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./StoriesFilter.css";
-import Paper from "@material-ui/core/Paper";
 import Checkbox from "@material-ui/core/Checkbox";
 import MenuItem from "@material-ui/core/MenuItem";
 import { MenuList } from "@material-ui/core";
@@ -8,131 +7,142 @@ import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 
-
 const lenses = [
-    "Refugee-Owned",
-    "Refugee-Led",
-    "Refugee-Supporting",
-    "Refugee-Supporting-Host-Weighted",
-    "Lending-Facilities",
-    "Refugee-Funds"
+  "Refugee-Owned",
+  "Refugee-Led",
+  "Refugee-Supporting",
+  "Refugee-Supporting-Host-Weighted",
+  "Lending-Facilities",
+  "Refugee-Funds"
 ];
 
 const SDGs = [
-    "Climate-Action",
-    "Decent-Work-and-Economic-Growth",
-    "Gender-Equality",
-    "Good-Health-and-Well-Being",
-    "Industry-Innovation-and-Infrastructure",
-    "Life-on-Land",
-    "No-Poverty",
-    "Partnerships-for-the-Goals",
-    "Peace-Justice-and-Strong-Institutions",
-    "Quality-Education",
-    "Reduced-Inqualities",
-    "Sustainable-Cities-and-Communities",
-    "Zero-Hunger"
+  "Climate-Action",
+  "Decent-Work-and-Economic-Growth",
+  "Gender-Equality",
+  "Good-Health-and-Well-Being",
+  "Industry-Innovation-and-Infrastructure",
+  "Life-on-Land",
+  "No-Poverty",
+  "Partnerships-for-the-Goals",
+  "Peace-Justice-and-Strong-Institutions",
+  "Quality-Education",
+  "Reduced-Inqualities",
+  "Sustainable-Cities-and-Communities",
+  "Zero-Hunger"
 ];
 
 export default class StoriesFilter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isFilterToggled: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFilterToggled: false
+    };
+  }
 
-    toggleFilter = () => {
-        this.setState({ isFilterToggled: !this.state.isFilterToggled });
-    }
+  toggleFilter = () => {
+    this.setState({ isFilterToggled: !this.state.isFilterToggled });
+  };
 
-    render() {
-        let show = this.state.isFilterToggled ? " show" : "";
+  render() {
+    let show = this.state.isFilterToggled ? " show" : "";
 
-        let allLenses = lenses.map((lens, i) => {
-            return (
-                <option value={lens} key={i}>{lens}</option>
-            );
-        });
+    let allLenses = lenses.map((lens, i) => {
+      return (
+        <option value={lens} key={i}>
+          {lens}
+        </option>
+      );
+    });
 
-        let allSDGs = SDGs.map((sdg, i) => {
-            return (
-                <MenuItem>
-                    <Checkbox
-                        checked={this.state.checked}
-                        style={{ color: "var(--color-2)" }}
-                        onChange={this.props.filterSDGs}
-                        value={i}
-                        key={i}
-                        name="SDG"
-                    />
-                    {sdg}
-                </MenuItem>
-            );
-        });
+    let allSDGs = SDGs.map((sdg, i) => {
+      return (
+        <MenuItem>
+          <Checkbox
+            checked={this.state.checked}
+            style={{ color: "var(--color-2)" }}
+            onChange={this.props.filterSDGs}
+            value={i}
+            key={i}
+            name="SDG"
+          />
+          {sdg}
+        </MenuItem>
+      );
+    });
 
-        return (
-            <div className={"stories-filter" + show}>
-                <a onClick={this.toggleFilter}>
-                    <i className="fas fa-search" /> Search
-                </a>
+    return (
+      <div className={"stories-filter" + show}>
+        <a onClick={this.toggleFilter}>
+          <i className="fas fa-search" /> Search
+        </a>
 
-                <a
-                    className="close"
-                    onClick={this.toggleFilter}
-                    style={{ display: this.state.isFilterToggled ? "block" : "none" }}>
-                    <i className="fas fa-times" />
-                </a>
+        <a
+          className="close"
+          onClick={this.toggleFilter}
+          style={{ display: this.state.isFilterToggled ? "block" : "none" }}
+        >
+          <i className="fas fa-times" />
+        </a>
 
-                <div className="stories-filter-input">
-                    <label htmlFor="story-title" className="stories-filter-label">
-                        Filter by Story Title
-                    </label>
-                    <input
-                        id="story-title"
-                        type="text"
-                        name="title"
-                        placeholder="story title"
-                        className="stories-filter-input-text"
-                        onChange={this.props.filter}
-                    />
-                </div>
+        <div className="stories-filter-input">
+          <label htmlFor="story-title" className="stories-filter-label">
+            Filter by Story Title
+          </label>
+          <input
+            id="story-title"
+            type="text"
+            name="title"
+            placeholder="story title"
+            className="stories-filter-input-text"
+            onChange={this.props.filter}
+          />
+        </div>
 
-                <div className="stories-filter-input">
-                    <label htmlFor="story-title" className="stories-filter-label">
-                        Filter by Refugee Lens
-                    </label>
-                    <select name="lens" id="lens" onChange={this.props.filter} className="stories-lens-select">
-                        <option>Select Lens</option>
-                        {allLenses}
-                    </select>
-                </div>
+        <div className="stories-filter-input">
+          <label htmlFor="story-title" className="stories-filter-label">
+            Filter by Refugee Lens
+          </label>
+          <select
+            name="lens"
+            id="lens"
+            onChange={this.props.filter}
+            className="stories-lens-select"
+          >
+            <option>Select Lens</option>
+            {allLenses}
+          </select>
+        </div>
 
-                <div className="stories-filter-input">
-                    <label htmlFor="story-title" className="stories-filter-label">
-                        Filter by SDGs
-                    </label>
-                    <ExpansionPanel className="stories-filter-SDGs">
-                        <ExpansionPanelSummary style={{ backgroundColor: "rgba(255, 153, 0, 0.8)", border: "1px solid #fff" }}>
-                            <p>Select SDGs</p>
-                        </ExpansionPanelSummary>
-                        <ExpansionPanelDetails>
-                            <MenuList>{allSDGs}</MenuList>
-                        </ExpansionPanelDetails>
-                    </ExpansionPanel>
-                </div>
+        <div className="stories-filter-input">
+          <label htmlFor="story-title" className="stories-filter-label">
+            Filter by SDGs
+          </label>
+          <ExpansionPanel className="stories-filter-SDGs">
+            <ExpansionPanelSummary
+              style={{
+                backgroundColor: "rgba(255, 153, 0, 0.8)",
+                border: "1px solid #fff"
+              }}
+            >
+              <p>Select SDGs</p>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <MenuList>{allSDGs}</MenuList>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
 
-                <button
-                    className="btn"
-                    onClick={() => {
-                        this.toggleFilter();
-                        this.props.fetchStories();
-                    }}
-                >
-                    Go
-                </button>
-
-            </div>
-        )
-    }
+        <button
+          className="btn"
+          onClick={() => {
+            this.toggleFilter();
+            this.props.fetchStories();
+          }}
+        >
+          Go
+        </button>
+      </div>
+    );
+  }
 }
