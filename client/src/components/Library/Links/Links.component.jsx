ee6aff0568bdc5885dbfeb7e3b5b7a8ca56aea0d
z3by <zoaby.am@gmail.com>
2018-11-25 14:17:39 +0200

    library create card

--- client/src/components/Library/Links/Links.component.jsx
+++ client/src/components/Library/Links/Links.component.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Card from "../Card/Card.component";
 
 export default class Links extends Component {
   render() {
     return (
       <div>
-        <h1> hello from links </h1>
+        <Card />
       </div>
     );
   }
