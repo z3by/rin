3cab3a4c6b9dde0de3e8db00276533c99292381a
z3by <zoaby.am@gmail.com>
2018-11-26 10:04:34 +0200

    library researches list

--- client/src/components/Library/Researches/Researches.component.jsx
+++ client/src/components/Library/Researches/Researches.component.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import ResearchList from "./ResearchList.component";
 
 export default class Researches extends Component {
   render() {
     return (
       <div>
-        <h1> hello from Researches </h1>
+        <ResearchList />
       </div>
     );
   }
