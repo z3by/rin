d69add57d2f66839f830fb752f002a553556e165
z3by <zoaby.am@gmail.com>
2018-11-26 11:02:43 +0200

    FIX add&update project bug

--- client/src/components/Dashboard/Projects/NewProject/NewProject.component.jsx
+++ client/src/components/Dashboard/Projects/NewProject/NewProject.component.jsx
@@ -38,7 +38,8 @@ export default class NewProject extends Component {
       lat: 0,
       zoom: 0,
       loading: false,
-      formValid: false
+      formValid: false,
+      uploaded: false
     };
   }
 
@@ -117,15 +118,21 @@ export default class NewProject extends Component {
     };
     axios
       .post("/api/projects", projectData)
-      .then(function(response) {
+      .then(response => {
         document.querySelector(".admin-form form").reset();
-        document.querySelector(".done-img").style.display = "flex";
+        this.setState({
+          uploaded: true,
+          formValid: false,
+          img_url: ""
+        });
+
         setTimeout(() => {
-          this.disableAddButton();
-          document.querySelector(".done-img").style.display = "none";
-        }, 2000);
+          this.setState({
+            uploaded: false
+          });
+        }, 3000);
       })
-      .catch(function(error) {
+      .catch(error => {
         console.log(error);
       });
   };
@@ -261,7 +268,10 @@ export default class NewProject extends Component {
           >
             <i className="fas fa-plus" /> Add Project
           </button>
-          <div className="done-img">
+          <div
+            className="done-img"
+            style={{ opacity: this.state.uploaded ? "1" : "0" }}
+          >
             <img src="/imgs/done.gif" alt="" />
           </div>
         </form>
