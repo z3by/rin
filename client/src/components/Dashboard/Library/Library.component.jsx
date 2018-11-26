42a1643e875006c2b4875b714a692861d6d7db80
z3by <zoaby.am@gmail.com>
2018-11-26 15:48:42 +0200

    init library dashboard routing

--- client/src/components/Dashboard/Library/Library.component.jsx
+++ client/src/components/Dashboard/Library/Library.component.jsx
@@ -1,15 +1,60 @@
 import React, { Component } from "react";
 import { Link, Route } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import LinksList from "./Links/LinksList.component";
+import BooksList from "./Books/BooksList.component";
+import ResearchesList from "./Researches/ResearchesList.component";
 
 export default class Library extends Component {
+  state = {
+    anchorEl: null
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const { anchorEl } = this.state;
     return (
       <div className="projects-dashboard fadeInFast">
         <nav className="nav-up">
-          <Link to="/dashboard/articles/add">
-            <i className="fas fa-plus" />
-            <span>New Article</span>
-          </Link>
+          <Button
+            aria-owns={anchorEl ? "simple-menu" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+          >
+            Sections
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleClose}
+          >
+            <MenuItem onClick={this.handleClose}>
+              <Link className="color-5" to="/dashboard/library/links">
+                Links
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={this.handleClose}>
+              <Link className="color-5" to="/dashboard/library/books">
+                Books
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={this.handleClose}>
+              <Link className="color-5" to="/dashboard/library/researches">
+                Researches
+              </Link>
+            </MenuItem>
+          </Menu>
+
           <div className="search-group">
             <input
               type="search"
@@ -19,7 +64,14 @@ export default class Library extends Component {
             <i className="fas fa-search" />
           </div>
         </nav>
-        <main>hello form library</main>
+        <main>
+          <Route path="/dashboard/library/links" component={LinksList} />
+          <Route path="/dashboard/library/books" component={BooksList} />
+          <Route
+            path="/dashboard/library/researches"
+            component={ResearchesList}
+          />
+        </main>
       </div>
     );
   }
---
+++ client/src/components/Dashboard/Library/Researches/ResearchesList.component.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ResearchesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from researches list</h1>
+      </div>
+    );
+  }
+}
