4eeeb6dbd77a49517f6112d95485c233cee8cc50
z3by <zoaby.am@gmail.com>
2018-11-27 16:10:01 +0200

    FIX routes names users controller

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,9 @@ router.post("/loginadmin", adminCtrl.loginAdmin);
 router.post("/register", membersCtrl.registerNewMember);
 router.post("/login", membersCtrl.loginMember);
 
-router.get("/all", adminCtrl.getAllMembers);
-router.get("/allusers/count", adminCtrl.getUsersCount);
+router.get("/users", adminCtrl.getAllMembers);
+router.get("/users/count", adminCtrl.getUsersCount);
 router.get("/allusers/search/:options", adminCtrl.getSearchedMembers);
-router.get("/allusers/selectedpage", adminCtrl.getSelectedPageUsers);
+router.get("/users/selectedpage", adminCtrl.getSelectedPageUsers);
 
 module.exports = router;
