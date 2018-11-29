6b053cd661a2017fe7695a11b79cd8735263ab5b
z3by <zoaby.am@gmail.com>
2018-11-29 10:32:21 +0200

    change model files names

---
+++ models/Link.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Link = sequelize.define(
+    "Link",
+    {
+      title: DataTypes.STRING,
+      subtitle: DataTypes.STRING,
+      url: DataTypes.STRING,
+      imgUrl: DataTypes.STRING
+    },
+    {}
+  );
+  Link.associate = function(models) {
+    // associations can be defined here
+  };
+  return Link;
+};
--- models/Location.js
+++ models/Location.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const location = sequelize.define('location', {
-    lng: DataTypes.FLOAT,
-    lat: DataTypes.FLOAT
-  }, {});
+  const location = sequelize.define(
+    "location",
+    {
+      lng: DataTypes.FLOAT,
+      lat: DataTypes.FLOAT
+    },
+    {}
+  );
   location.associate = function(models) {
     // associations can be defined here
   };
