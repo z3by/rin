520d9ba4a9a976f73ecd3540acc681473e08434f
z3by <zoaby.am@gmail.com>
2018-12-05 16:41:03 +0200

    UDPATE contries routes

--- controllers/api/countries.controller.js
+++ controllers/api/countries.controller.js
@@ -1,13 +1,35 @@
 const countriesData = require("../../data/countries.json");
+const db = require("../../models/index");
 
 module.exports.getCountries = (req, res) => {
-  res.json(countriesData);
+  db.Country.findAll().then(result => {
+    res.status(200).json(result);
+  });
 };
 
 module.exports.getCountriesNames = (req, res) => {
-  const countries = [];
-  countriesData.forEach(country => {
-    countries.push(country.name);
+  db.Country.findAll().then(result => {
+    const countries = [];
+    result.forEach(country => {
+      countries.push(country.name);
+    });
+    res.status(200).json(countries);
   });
-  res.json(countries);
+};
+
+module.exports.fillCountryTable = (req, res) => {
+  let i = 1;
+  db.Country.count().then(count => {
+    if (!!count) {
+      return;
+    }
+  });
+  countriesData.forEach(country => {
+    db.Country.create(country).then(created => {
+      i++;
+      if (i >= countriesData.length) {
+        res.send(201);
+      }
+    });
+  });
 };
