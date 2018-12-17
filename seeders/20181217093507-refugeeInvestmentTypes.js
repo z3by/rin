c403edcc0435b814660e4503d00f17545a776316
z3by <zoaby.am@gmail.com>
2018-12-17 12:25:17 +0200

    setup refugee investment types seeder

---
+++ seeders/20181217093507-refugeeInvestmentTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+const refugeeInvestmentTypesData = require("../data/refugeeInvestmentTypes");
+const db = require("../models");
+
+const filterData = refugeeInvestmentTypesData.map(rft => {
+  return {
+    name: rft.name,
+    img: rft.img,
+    createdAt: new Date(),
+    UpdatedAt: new Date()
+  };
+});
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("refugeeInvestmentTypes", filterData, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("refugeeInvestmentTypes", null, {});
+  }
+};
