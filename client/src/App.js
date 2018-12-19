a6bdae1496cea5c5ded014d62456f699b06eeaa0
z3by <zoaby.am@gmail.com>
2018-12-19 16:57:05 +0200

    FIX login form style

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
               <a
                 href="https://www.linkedin.com/company/refugee-investment-network/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab fa-linkedin-in" />
               </a>
@@ -29,6 +30,7 @@ class App extends Component {
               <a
                 href="https://twitter.com/refugeeinvest?lang=en"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab fa-twitter" />
               </a>
