83b56b40744494ca78caf184dc533c583d0c2f48
z3by <zoaby.am@gmail.com>
2018-12-24 17:05:48 +0200

    REFACTOR update the requests list

--- client/src/components/Dashboard/Requests/Requests.component.jsx
+++ client/src/components/Dashboard/Requests/Requests.component.jsx
@@ -1,26 +1,12 @@
-import React, { Component } from "react";
-
-import RequestList from "./RequestList.component";
+import React from "react";
 import "./requests.css";
+import RequestList from "./RequestList.component";
 
-export default class Requests extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projectRequests: [],
-      articleRequests: []
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <RequestList
-          handleAccept={this.handleAccept}
-          requests={this.props.projectRequests}
-          itemName={"project"}
-        />
-      </div>
-    );
-  }
+export default function Requests() {
+  return (
+    <div>
+      <RequestList itemName={"project"} />
+      <RequestList itemName={"article"} />
+    </div>
+  );
 }
