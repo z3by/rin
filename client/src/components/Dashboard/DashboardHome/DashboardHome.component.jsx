71c5e703691885d3e4343d7603807d3915a96b98
z3by <zoaby.am@gmail.com>
2018-12-26 16:27:44 +0200

    UPDATE dashobard home  style

--- client/src/components/Dashboard/DashboardHome/DashboardHome.component.jsx
+++ client/src/components/Dashboard/DashboardHome/DashboardHome.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Typography, Card } from "@material-ui/core";
 import Axios from "axios";
 
 export default class DashboardHome extends Component {
@@ -14,15 +14,23 @@ export default class DashboardHome extends Component {
   render() {
     return (
       <div className="container">
-        <div className="projects-statistics">
-          <Typography variant="h4" className="text-center color-3 py-10">
-            RIN Projects Statistics
-          </Typography>
-          <Card className="col">
-            <CardContent>
-              <Doughnut data={this.state.data} />
-            </CardContent>
-          </Card>
+        <div className="">
+          <div
+            className="projects-statistics"
+            style={{
+              minHeight: 400,
+              padding: 20
+            }}
+          >
+            <Typography
+              variant="h4"
+              style={{ paddingBottom: 30 }}
+              className="text-center color-5 py-10"
+            >
+              RIN Projects Statistics
+            </Typography>
+            <Doughnut data={this.state.data} />
+          </div>
         </div>
       </div>
     );
