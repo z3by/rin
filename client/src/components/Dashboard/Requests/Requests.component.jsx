96e3046d4ddc5449201c47fc72381a0dc725c348
z3by <zoaby.am@gmail.com>
2018-12-26 09:57:17 +0200

    DELETE requestList component&request card comp

--- client/src/components/Dashboard/Requests/Requests.component.jsx
+++ client/src/components/Dashboard/Requests/Requests.component.jsx
@@ -1,12 +1,66 @@
 import React from "react";
 import "./requests.css";
-import RequestList from "./RequestList.component";
 
-export default function Requests() {
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+function TabContainer(props) {
   return (
-    <div>
-      <RequestList itemName={"project"} />
-      <RequestList itemName={"article"} />
-    </div>
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
   );
 }
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  }
+});
+
+class Requests extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            className="flex-centerd bg-5"
+          >
+            <Tab label="Project Requests" />
+            <Tab label="Article Requests" />
+          </Tabs>
+        </AppBar>
+
+        {value === 0 && <TabContainer>project requests</TabContainer>}
+        {value === 1 && <TabContainer>article requests</TabContainer>}
+      </div>
+    );
+  }
+}
+
+Requests.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Requests);
