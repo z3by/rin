35abf03eaf99402ae275c857dca9dd024c6ef1ac
ShSukkar <sukkarsmiling89@hotmail.com>
2018-12-26 16:43:05 +0200

    UPDATE dynamic admin info

---
+++ data/adminInfo.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "firstName": "RIN",
+    "lastName": "RIN",
+    "email": "rin@rin.com",
+    "organizationName": "RIN",
+    "isSuperUser": true,
+    "password": "RIN123"
+};
