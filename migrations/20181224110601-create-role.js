b4a0cf24f10221732c58c7db9d0e6109eb25eb4e
z3by <zoaby.am@gmail.com>
2018-12-30 14:42:13 +0200

    FIX tables names

--- migrations/20181224110601-create-role.js
+++ migrations/20181224110601-create-role.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Roles', {
+    return queryInterface.createTable('roles', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Roles');
+    return queryInterface.dropTable('roles');
   }
 };
--- migrations/20181224114418-create-permission.js
+++ migrations/20181224114418-create-permission.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Permissions', {
+    return queryInterface.createTable('permissions', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Permissions');
+    return queryInterface.dropTable('permissions');
   }
 };
