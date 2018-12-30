f82960e16ec106a57f93599e3a2f27e0df074f16
z3by <zoaby.am@gmail.com>
2018-12-30 13:55:21 +0200

    UPDATE member forms markup

--- client/src/components/SignUpLogIn/SignUpLogIn.component.jsx
+++ client/src/components/SignUpLogIn/SignUpLogIn.component.jsx
@@ -1,323 +1,187 @@
 import React, { Component } from "react";
-import axios from "axios";
 import "./SignUpLogIn.css";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import {
+  Card,
+  CardContent,
+  Typography,
+  TextField,
+  Button,
+  CardActions,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel
+} from "@material-ui/core";
 
-export default class SignUpLogIn extends Component {
+const SignupForm = props => {
+  return (
+    <CardContent className="box-form">
+      <TextField
+        className="box-input color-3"
+        label="Email"
+        InputLabelProps={{ shrink: true }}
+        variant="outlined"
+        name="email"
+        type="email"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="First Name"
+        InputLabelProps={{ shrink: true }}
+        variant="outlined"
+        name="firstName"
+        type="text"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="Last Name"
+        InputLabelProps={{ shrink: true }}
+        variant="outlined"
+        name="lastName"
+        type="text"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="Organization Name"
+        InputLabelProps={{ shrink: true }}
+        variant="outlined"
+        name="organizationName"
+        type="text"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="Password"
+        type="password"
+        InputLabelProps={{ shrink: true }}
+        name="password"
+        variant="outlined"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="Confirm Password"
+        type="password"
+        InputLabelProps={{ shrink: true }}
+        name="password2"
+        variant="outlined"
+        required
+        value={"hello"}
+      />
+      <FormControl className="box-input">
+        <InputLabel htmlFor="role">User Role</InputLabel>
+        <Select
+          value={1}
+          onChange={this.handleChange}
+          inputProps={{ name: "role", id: "role" }}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={1}>Entrepreneur</MenuItem>
+          <MenuItem value={2}>Investor</MenuItem>
+          <MenuItem value={3}>humanitarian community</MenuItem>
+        </Select>
+      </FormControl>
+    </CardContent>
+  );
+};
+
+const LoginForm = props => {
+  return (
+    <CardContent className="box-form">
+      <TextField
+        className="box-input color-3"
+        label="Email"
+        InputLabelProps={{ shrink: true }}
+        name="email"
+        type="email"
+        variant="outlined"
+        required
+        value={"hello"}
+      />
+      <TextField
+        className="box-input color-3"
+        label="Password"
+        type="password"
+        InputLabelProps={{ shrink: true }}
+        name="password"
+        variant="outlined"
+        required
+        value={"hello"}
+      />
+    </CardContent>
+  );
+};
+
+export default class MemberLogin extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      roles: ["Investor", "Entrepreneur", "Humanitarian Community"],
-      userInfo: {
-        firstName: "",
-        lastName: "",
-        organizationName: "",
-        userRole: "",
-        email: "",
-        password: "",
-        password2: ""
-      },
-      errors: []
+      loginCard: true
     };
   }
 
-  // display the proper form for the user (login || signup)
-  toggleClassBounce = () => {
-    if (
-      document
-        .querySelector(".user_options-forms")
-        .classList.contains("bounceRight")
-    ) {
-      document
-        .querySelector(".user_options-forms")
-        .classList.remove("bounceRight");
-      document.querySelector(".user_options-forms").classList.add("bounceLeft");
-      document.querySelector(".user_options-forms").style.height = "600px";
-    } else {
-      document
-        .querySelector(".user_options-forms")
-        .classList.remove("bounceLeft");
-      document
-        .querySelector(".user_options-forms")
-        .classList.add("bounceRight");
-      document.querySelector(".user_options-forms").style.height = "350px";
-
-    }
-  };
-
-  onChange = e => {
-    this.setState({
-      userInfo: {
-        ...this.state.userInfo,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  addUser = e => {
-    e.preventDefault();
-
-    let { userInfo } = this.state;
-
-    axios
-      .post("/users/register", userInfo)
-      .then(res => {
-        this.toggleClassBounce();
-      })
-      .catch(error => {
-        const errors = [];
-        if (typeof error.response.data === "object") {
-          for (let i in error.response.data) {
-            errors.push(<li key={i}>{error.response.data[i]}</li>);
-          }
-        }
-        this.setState({
-          errors: errors
-        });
-      });
-  };
-
-  login = e => {
-    e.preventDefault();
-
-    let userData = {
-      email: this.state.userInfo.email,
-      password: this.state.userInfo.password
-    };
-
-    axios
-      .post("/users/login", userData)
-      .then(res => {
-        const token = res.data.token;
-        window.localStorage.setItem("jwttoken", token);
-        this.props.history.push("members");
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  toggleErrors = () => {
-    this.setState({ errors: [] });
+  toggleForm = () => {
+    this.setState({ loginCard: !this.state.loginCard });
   };
 
   render() {
-    let roles = this.state.roles.map((role, i) => {
-      return (
-        <MenuItem value={role} key={i}>
-          {" "}
-          {role}
-        </MenuItem>
-      );
-    });
-
     return (
-      <div className="user">
-        <div
-          className="errors"
-          style={{ display: this.state.errors.length ? "block" : "none" }}
-        >
-          <h3 className="heading-theme-2">please try again</h3>
-          <ul>{this.state.errors}</ul>
-          <button className="errors-dismiss" onClick={this.toggleErrors}>
-            OK
-          </button>
-        </div>
-        <div className="user_options-container">
-          <div className="user_options-text">
-            <div className="user_options">
-              <h2 className="user-title">Don't have an account?</h2>
-              <p className="user-text">
-                Create a member account and enjoy our exclusive services
-              </p>
-              <button
-                className="user-signup-login"
-                onClick={this.toggleClassBounce}
-              >
-                Sign up
-              </button>
-            </div>
-
-            <div className="user_options">
-              <h2 className="user-title">Have an account?</h2>
-              <p className="user-text">Login dierctly to your account</p>
-              <button
-                className="user-signup-login"
-                onClick={this.toggleClassBounce}
-              >
-                Login
-              </button>
-            </div>
+      <div className="members-login-bg">
+        <Card className="box">
+          <div className="box-header bg-2 text-center">
+            <i className="fas fa-user box-header-icon" />
+            <Typography
+              variant="h6"
+              className="text-center"
+              style={{ display: this.state.loginCard ? "block" : "none" }}
+            >
+              Login To Members Area
+            </Typography>
+            <Typography
+              variant="h6"
+              className="text-center"
+              style={{ display: this.state.loginCard ? "none" : "block" }}
+            >
+              Create New Account
+            </Typography>
           </div>
-          {/******************** login form  *******************************/}
-          <div className="user_options-forms bounceRight">
-            <div className="user_forms-login">
-              <Typography variant="h2" className="forms_title">
-                Login
-              </Typography>
-              <FormControl className="forms_form">
-                <fieldset className="forms_fieldset">
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Email"
-                      type="email"
-                      name="email"
-                      autoComplete="email"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.email}
-                      placeholder="Email"
-                      style={{ marginBottom: "5px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Password"
-                      type="password"
-                      name="password"
-                      autoComplete="current-password"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.password}
-                      style={{ height: "48px" }}
-                      required
-                    />
-                  </div>
-                </fieldset>
-                <div className="forms_buttons">
-                  <Button
-                    variant="contained"
-                    onClick={this.login}
-                    style={{ color: "var(--color-2)" }}
-                  >
-                    Login
-                  </Button>
-                </div>
-              </FormControl>
+          {this.state.loginCard ? (
+            <LoginForm toggleForm={this.toggleForm} />
+          ) : (
+            <SignupForm toggleForm={this.toggleForm} />
+          )}
+          <CardActions className="card-actions">
+            <Button className="box-btn">Sign Up</Button>
+            <div className="flex justify-content-between w-100">
+              <a
+                onClick={this.toggleForm}
+                style={{ display: this.state.loginCard ? "block" : "none" }}
+              >
+                <small className="color-2">Create New account ?</small>
+              </a>
+              <a
+                onClick={this.toggleForm}
+                style={{ display: this.state.loginCard ? "none" : "block" }}
+              >
+                <small className="color-2">Already Have An Account ?</small>
+              </a>
+              <a>
+                <small className="color-2">forget password?</small>
+              </a>
             </div>
-
-            {/******************** signup form  *******************************/}
-            <div className="user_forms-signup">
-              <Typography variant="h2" className="forms_title">
-                Signup
-              </Typography>
-              <FormControl className="forms_form" onSubmit={this.addUser}>
-                <fieldset className="forms_fieldset">
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="First Name"
-                      name="firstName"
-                      autoComplete="firstName"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.firstName}
-                      style={{ marginBottom: "5px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Last Name"
-                      name="lastName"
-                      autoComplete="lastName"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.lastName}
-                      style={{ marginBottom: "5px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Organization Name"
-                      name="organizationName"
-                      autoComplete="organizationName"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.organizationName}
-                      style={{ marginBottom: "5px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <FormControl style={{ width: "100%" }}>
-                      <InputLabel htmlFor="role-select">select role</InputLabel>
-                      <Select
-                        value={this.state.userInfo.userRole}
-                        onChange={this.onChange}
-                        inputProps={{
-                          name: "userRole",
-                          id: "role-select",
-                          value: this.state.userInfo.userRole
-                        }}
-                      >
-                        {roles}
-                      </Select>
-                    </FormControl>
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Email"
-                      type="email"
-                      name="email"
-                      autoComplete="email"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.email}
-                      placeholder="Email"
-                      style={{ marginBottom: "5px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Password"
-                      type="password"
-                      name="password"
-                      autoComplete="current-password"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.password}
-                      style={{ height: "48px" }}
-                      required
-                    />
-                  </div>
-                  <div className="forms_field">
-                    <TextField
-                      className="full-width-input"
-                      label="Confirm Password"
-                      type="password"
-                      name="password2"
-                      autoComplete="current-password"
-                      onChange={this.onChange}
-                      value={this.state.userInfo.password2}
-                      style={{ height: "48px" }}
-                      required
-                    />
-                  </div>
-                </fieldset>
-                <div className="forms_buttons">
-                  <Button
-                    variant="contained"
-                    onClick={this.addUser}
-                    style={{ color: "var(--color-2)" }}
-                  >
-                    Signup
-                  </Button>
-                </div>
-              </FormControl>
-            </div>
-          </div>
-        </div>
-      </div >
+          </CardActions>
+        </Card>
+      </div>
     );
   }
 }
